08bb8e4495189a6df84a2b9f81e723fca4d2a504
Vlad <vladanalyst@gmail.com>
2024-07-25 02:17:05 +0300

    задание 18.4 выполнено

---
+++ src/moderation/moderation.processor.ts
@@ -0,0 +1,24 @@
+import { Process, Processor} from '@nestjs/bull';
+import { Job } from 'bull';
+
+@Processor('moderation')
+export class ModerationProcessor {
+    @Process('moderate')
+    async moderate(job: Job) {
+        const start = Date.now();
+        console.log('Handling started');
+        
+        for (let i = 0; i <= 100; i++) {
+            console.log(`Progress: ${i}%`);
+            await job.progress(i);
+
+            for (let j = 0; j <= 100000; j++) {
+                for (let k = 0; k < 5000; k++) {
+                    j + i;
+                }
+            }
+        }
+        console.log(`Handling finished in ${Date.now() - start}ms`);
+        return {};
+    }
+}
