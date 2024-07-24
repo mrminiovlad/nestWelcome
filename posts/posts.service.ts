a47d683436edfd830bef6820a8aa66577e0b7c25
Vlad <vladanalyst@gmail.com>
2024-07-24 19:19:02 +0300

    задание 17.2 выполнено

---
+++ posts/posts.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { CreatePostDto } from './dto/create-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
+import { Post } from './entities/post.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import * as moment from 'moment';
+
+@Injectable()
+export class PostsService {
+  constructor(
+    @InjectRepository(Post)
+    private repository: Repository<Post>
+  ) { }
+
+  create(data: CreatePostDto) {
+    return this.repository.save({
+      ...data,
+      changed_at: moment().format('YYYY-MM-DD HH:mm:ss')
+    });
+  }
+  
+
+  findAll() {
+    return this.repository.find();
+  }
+
+  findOne(id: number) {
+    return this.repository.findOneBy({ id });
+  }
+
+  update(id: number, data: UpdatePostDto) {
+    return this.repository.save({...data, id});
+  }
+
+  async remove(id: number) {
+    await this.repository.delete(id);
+  }
+}
